5499bb5530b40664e72c5fd54273324c542fdc9e
saurabhwork1 <saurav818079@gmail.com>
2026-06-12 14:56:48 +0530

    Update clipper.py

--- clipper.py
+++ clipper.py
@@ -1,1 +1,11 @@
+import subprocess
 
+def split_video(video_file):
+    subprocess.run([
+        "ffmpeg",
+        "-i", video_file,
+        "-f", "segment",
+        "-segment_time", "60",
+        "-c", "copy",
+        "clips/clip_%03d.mp4"
+    ])
