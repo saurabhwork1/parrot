56bb7d65027cd95b191531a495a82cf6331eb47a
saurabhwork1 <saurav818079@gmail.com>
2026-06-12 15:04:22 +0530

    Update clipper.py

--- clipper.py
+++ clipper.py
@@ -1,11 +1,34 @@
+import os
 import subprocess
+from config import CLIP_DURATION, CLIPS_FOLDER
+
 
 def split_video(video_file):
+    os.makedirs(CLIPS_FOLDER, exist_ok=True)
+
+    output_pattern = f"{CLIPS_FOLDER}/clip_%03d.mp4"
+
     subprocess.run([
         "ffmpeg",
-        "-i", video_file,
-        "-f", "segment",
-        "-segment_time", "60",
-        "-c", "copy",
-        "clips/clip_%03d.mp4"
+        "-i",
+        video_file,
+        "-c",
+        "copy",
+        "-map",
+        "0",
+        "-f",
+        "segment",
+        "-segment_time",
+        str(CLIP_DURATION),
+        output_pattern
     ])
+
+    clips = []
+
+    for file in os.listdir(CLIPS_FOLDER):
+        if file.endswith(".mp4"):
+            clips.append(os.path.join(CLIPS_FOLDER, file))
+
+    clips.sort()
+
+    return clips
