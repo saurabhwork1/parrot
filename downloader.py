6b41dae92d0f86ea88af8196a22ebe251495e2f8
saurabhwork1 <saurav818079@gmail.com>
2026-06-12 14:55:57 +0530

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,1 +1,11 @@
+from yt_dlp import YoutubeDL
 
+def download_video(url):
+    ydl_opts = {
+        "outtmpl": "video.%(ext)s"
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    return "video.mp4"
